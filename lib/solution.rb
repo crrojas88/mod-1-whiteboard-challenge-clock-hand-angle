0edacdffc5f47869227ce3d05af6fcc6d36a6f1a
crrojas88 <carlorrojas@gmail.com>
2020-10-20 09:51:35 -0700

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    time_array = parse_time(time)
+    hour = time_array[0]
+    min = time_array[1]
+    min_angle =  min * 6
+    hour_angle = hour % 12 * 30 + min.to_f/2
+    if hour_angle < min_angle
+        hour_angle += 360
+    end
+    
+    hour_angle - min_angle
+   
 end
+
+def parse_time(time)
+    min = time[time.length - 2, time.length]
+    hour = time[0, time.length - 3]
+     [hour.to_i, min.to_i]
+end
+
+# parse_time("3:15")
+# #clock_angle(3:15)
